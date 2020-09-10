5cb07a62b929e9691a756b721533d413f8110840
urmil <ucodes28@gmail.com>
2020-09-10 01:45:36 -0500

    appending number value to the label on button tap

--- Calculator-Storyboard/ViewController.swift
+++ Calculator-Storyboard/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonClick(_ sender: UIButton) {
+        
+        var number = sender.titleLabel!.text!
+        //at button click number should appear at the console.
+        //print(sender.titleLabel!.text!)
+        displayLabel.text! += number
+        
+        
+        
+    }
 }
 
